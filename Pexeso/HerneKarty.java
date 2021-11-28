6881e537ac64651dba405d748a548d797db078ef
Jakub Simek <simek.jakubs@gmail.com>
2021-11-28 17:04:04 +0100

    First commit

---
+++ Pexeso/HerneKarty.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class HerneKarty here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HerneKarty {
+    /**
+     * Constructor for objects of class HerneKarty
+     */
+    public HerneKarty() {
+        // initialise instance variables
+    }
+}
